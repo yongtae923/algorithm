1a8c3bf70ad3c01ead4be27c64589c110ec233c0
yongtae <yongtae@kaist.ac.kr>
2021-07-06 22:17:36 +0900

    how to get input?

---
+++ 2-1000 a+b.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("username is :" + name);
+        }
+    }
+}
