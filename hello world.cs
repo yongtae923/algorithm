e186473ce4d22d42744dc87f223491d02a3b7b06
yongtae <yongtae@kaist.ac.kr>
2021-07-06 13:15:26 +0900

    second commit

--- hello world.cs
+++ hello world.cs
@@ -7,6 +7,7 @@ namespace myproject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello again!");
         }
     }
 }
