0aac1dca19b751fd636cd7b542507d746930f2f5
yongtae <yongtae@kaist.ac.kr>
2021-08-21 16:46:46 +0900

    boj 2577

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int sum = a * b * c;
+            char[] sumChar = sum.ToString().ToCharArray();
+            List<int> counts = new List<int>(new int[10]);
+
+            foreach (char s in sumChar)
+            {
+                counts[int.Parse(s.ToString())]++;
+            }
+
+            foreach (int i in counts)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
