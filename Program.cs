a0cdcf8d2807d9ffff541c76ee85b323f3268879
yongtae <yongtae@kaist.ac.kr>
2021-08-21 17:18:31 +0900

    Fix gitignore

--- Program.cs
+++ Program.cs
@@ -6,22 +6,7 @@ namespace algorithm
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            int c = int.Parse(Console.ReadLine());
-            int sum = a * b * c;
-            char[] sumChar = sum.ToString().ToCharArray();
-            List<int> counts = new List<int>(new int[10]);
-
-            foreach (char s in sumChar)
-            {
-                counts[int.Parse(s.ToString())]++;
-            }
-
-            foreach (int i in counts)
-            {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine("Hello World");
         }
     }
 }
